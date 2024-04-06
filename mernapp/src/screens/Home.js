53d8beb5e98e6b5357c0c18fedfa3a335d714d98
vedikasharma28014 <vedikasharma2801@gmail.com>
2024-04-06 19:37:59 +0530

    navbar

---
+++ mernapp/src/screens/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+function Home() {
+  return (
+    <>
+    <div> <Navbar/> </div>
+    <div>Body</div>
+    <div><Footer/></div>
+    </>
+  )
+}
+export default Home;
